3854e0feabf45a771c882146fb9eed308b6c3d2b
Razvan Rujoi <razvanrujoi@yahoo.com>
2025-08-28 13:50:21 +0200

    feat: add makefile components

---
+++ StartGccBuildEnvironment.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: *************************************************************************************************
+:: Please do NOT change the following settings
+:: *************************************************************************************************
+set PlatformName=STM32H7B3-Discovery
+set GettingStartedLink=getting-started-stm32h7B3-discovery
+cmd /K cmake\devenv.cmd
